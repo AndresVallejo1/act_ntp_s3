03809f97ce302acd5f10b734f8d7e75570477dff
Isaac-max-bit <garciaalejoisaac@gmail.com>
2025-07-18 20:49:03 -0500

    Ejercicio 8

---
+++ src/ejercicio_07.py
@@ -0,0 +1,7 @@
+texto = "Manzana"
+contador = 0
+
+for letra in texto:
+    if letra == "a":
+        contador += 1
+print(f"La letra 'a' se ha encontrado {contador} veces hasta ahora.")
---
+++ src/ejercicio_08.py
@@ -0,0 +1,5 @@
+Numero = 1
+print("Presentaremos los numeros al cuadrado del 1 al 20")
+while Numero <= 20:
+    print(f"{Numero} al cuadrado es {Numero ** 2}")
+    Numero += 1
